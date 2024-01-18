aca902b91b05d39d478cf14f5953bc9657a1a8e6
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2024-01-18 17:45:35 -0300

    refactor: finalizando caso de uso de atualização das categorias

--- src/modules/categories/useCases/updateCategory/UpdateCategoryUseCase.spec.ts
+++ src/modules/categories/useCases/updateCategory/UpdateCategoryUseCase.spec.ts
@@ -24,31 +24,36 @@ describe('UpdateCategoryUseCase', () => {
   })
 
   it('the category must contain a minimum of 3 letters and a maximum of 30', async () => {
-    await expect(updateCategoryUseCase.execute(category_id, { name: '01' })).rejects.toThrow(ZodError)
+    await expect(updateCategoryUseCase.execute({ id: category_id, data: { name: '01' } })).rejects.toThrow(ZodError)
 
     await expect(
-      updateCategoryUseCase.execute(category_id, { name: '0123456789 ABCDEFGHIJKLMNOPQRSTUVZWYZ' })
+      updateCategoryUseCase.execute({ id: category_id, data: { name: '0123456789 ABCDEFGHIJKLMNOPQRSTUVZWYZ' } })
     ).rejects.toThrow(ZodError)
   })
 
   it('should return an error if an invalid uuid is entered', async () => {
-    await expect(updateCategoryUseCase.execute('invalid_uuid', { name: 'category_name' })).rejects.toThrow(ZodError)
+    await expect(
+      updateCategoryUseCase.execute({ id: 'invalid_uuid', data: { name: 'category_name' } })
+    ).rejects.toThrow(ZodError)
   })
 
   it('should return an error if the category was not found', async () => {
-    await expect(updateCategoryUseCase.execute(category_id, { name: 'category_name' })).rejects.toEqual(
-      new AppError('Category not found')
-    )
+    await expect(
+      updateCategoryUseCase.execute({
+        id: 'cc9c8edf-d252-453f-b362-ae75ce1dc9cb',
+        data: { name: 'category_inexistent' }
+      })
+    ).rejects.toEqual(new AppError('Category not found'))
   })
 
   it('should not be possible to update a category with an existing name', async () => {
-    await expect(updateCategoryUseCase.execute(category_id, { name: 'category_name' })).rejects.toEqual(
+    await expect(updateCategoryUseCase.execute({ id: category_id, data: { name: 'category_name' } })).rejects.toEqual(
       new AppError('Category already exists')
     )
   })
 
   it('should be possible to update a category', async () => {
-    const category = await updateCategoryUseCase.execute(category_id, { name: 'new_category_name' })
+    const category = await updateCategoryUseCase.execute({ id: category_id, data: { name: 'new_category_name' } })
     expect(category.name).toEqual('new_category_name')
   })
 })
--- src/modules/categories/useCases/updateCategory/UpdateCategoryUseCase.ts
+++ src/modules/categories/useCases/updateCategory/UpdateCategoryUseCase.ts
@@ -1,9 +1,32 @@
+import { z } from 'zod'
+
 import { ICategoriesRepository } from '@modules/categories/domains/repositories/ICategoriesRepository'
 import { IUpdateCategoryDTO } from '@modules/categories/domains/DTOs/categoriesDTOs'
 import { ICategory } from '@modules/categories/domains/models/ICategory'
+import { errors } from '@shared/errors/constants'
+import { validationUpdateCategory } from '@modules/categories/validations/validationCategories'
+import { AppError } from '@shared/errors/AppError'
+
+const validationId = z.object({ id: z.string().uuid(errors.id) })
+
+interface IRequest {
+  data: IUpdateCategoryDTO
+  id: string
+}
 
 export class UpdateCategoryUseCase {
   constructor(private readonly categoryRepository: ICategoriesRepository) {}
 
-  async execute(id: string, data: IUpdateCategoryDTO): Promise<ICategory> {}
+  async execute({ data, id }: IRequest): Promise<ICategory> {
+    const valid_data = validationUpdateCategory.parse(data)
+    const { id: category_id } = validationId.parse({ id })
+
+    const category = await this.categoryRepository.show(category_id)
+    if (!category) throw new AppError('Category not found')
+
+    const categoryExist = await this.categoryRepository.findAll({ name: valid_data.name, page_size: 1, page: 1 })
+    if (categoryExist.results.length > 0) throw new AppError('Category already exists')
+
+    return await this.categoryRepository.update(category_id, valid_data)
+  }
 }

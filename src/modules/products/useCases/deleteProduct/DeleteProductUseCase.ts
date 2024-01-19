9cb3ca9640ec94c00f33ac4f8597cac31abdf328
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2024-01-19 14:27:09 -0300

    refactor: finalizando caso de uso de exclusão do produto

--- src/modules/products/useCases/deleteProduct/DeleteProductUseCase.ts
+++ src/modules/products/useCases/deleteProduct/DeleteProductUseCase.ts
@@ -1,4 +1,10 @@
+import { z } from 'zod'
+
 import { IProductsRepository } from '@modules/products/domains/repositories/IProductsRepository'
+import { AppError } from '@shared/errors/AppError'
+import { errors } from '@shared/errors/constants'
+
+const validationId = z.object({ id: z.string().uuid(errors.id) })
 
 interface IResponse {
   message: string
@@ -8,6 +14,13 @@ export class DeleteProductUseCase {
   constructor(private readonly productsRepository: IProductsRepository) {}
 
   async execute(id: string): Promise<IResponse> {
+    const { id: product_id } = validationId.parse({ id })
+
+    const product = await this.productsRepository.show(product_id)
+    if (!product) throw new AppError('Product not found!', 404)
+
+    await this.productsRepository.delete(product_id)
+
     return { message: 'Product removed successfully!' }
   }
 }

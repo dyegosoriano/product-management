0771b5f6afffa01806593ea63cb5413f79223005
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2024-01-19 16:07:58 -0300

    refactor: finalizando caso de uso de listagem de produtos

--- src/modules/products/useCases/listProducts/ListProductsUseCase.ts
+++ src/modules/products/useCases/listProducts/ListProductsUseCase.ts
@@ -1,11 +1,23 @@
 import { IProductsRepository } from '@modules/products/domains/repositories/IProductsRepository'
+import { validationListProducts } from '@modules/products/validations/validationProducts'
+import { IFindAllProductsDTO } from '@modules/products/domains/DTOs/productsDTOs'
 import { ICategory } from '@modules/categories/domains/models/ICategory'
 import { IResultList } from '@core/types/utils/IResultList'
-
-interface IRequest {}
 
 export class ListProductsUseCase {
   constructor(private readonly productsRepository: IProductsRepository) {}
 
-  async execute(data: IRequest): Promise<IResultList<ICategory>> {}
+  async execute(data: IFindAllProductsDTO): Promise<IResultList<ICategory>> {
+    const valid_data = validationListProducts.parse(data)
+
+    const { total, results } = await this.productsRepository.findAll(valid_data)
+
+    return {
+      total_pages: Math.ceil(total / valid_data.page_size),
+      total: total,
+      page_size: +valid_data.page_size,
+      page: +valid_data.page,
+      results
+    }
+  }
 }

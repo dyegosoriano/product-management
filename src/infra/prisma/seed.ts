2a5ade95458b22225718472b603c70f333dc7e09
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2024-01-21 15:21:18 -0300

    feat: criação da seed com categorias iniciais

--- src/infra/prisma/seed.ts
+++ src/infra/prisma/seed.ts
@@ -1,1 +1,35 @@
-console.log('Running seed')
+import { PrismaClient } from '@prisma/client'
+
+import { Category } from '@modules/categories/entities/Category'
+
+const categories = [
+  { percentage: 0.025, name: 'automotive' },
+  { percentage: 0.01, name: 'furniture' },
+  { percentage: 0.05, name: 'computing' }
+]
+
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    for await (const item of categories) {
+      const category = new Category() as any
+      Object.assign(category, { ...item })
+
+      const admin = await prisma.categories.upsert({
+        where: { name: category.name },
+        create: category,
+        update: category
+      })
+    }
+
+    await prisma.$disconnect()
+  } catch (error) {
+    console.error(error)
+
+    await prisma.$disconnect()
+    process.exit(1)
+  }
+}
+
+main()

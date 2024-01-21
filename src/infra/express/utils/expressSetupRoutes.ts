732694ad2489c54c4f24cf8de449e04c07888983
Dyego Soriano <4140145+dyegosoriano@users.noreply.github.com>
2024-01-21 15:43:24 -0300

    fix: correção do auto import de rotas no ambiente de produção

--- src/infra/express/utils/expressSetupRoutes.ts
+++ src/infra/express/utils/expressSetupRoutes.ts
@@ -2,8 +2,15 @@ import { Application } from 'express'
 import filesystem from 'node:fs'
 import path from 'node:path'
 
+const is_dev = process.env.NODE_ENV === 'development'
+
+const path_dev = path.resolve(__dirname, '..', '..', '..', 'modules')
+const path_prod = path.resolve(__dirname, '..', 'modules')
+
+const file_extension = is_dev ? '.routes.ts' : '.routes.js'
+
 export const expressSetupRoutes = (app: Application) => {
-  const directory = path.resolve(__dirname, '..', '..', '..', 'modules')
+  const directory = is_dev ? path_dev : path_prod
 
   filesystem.readdirSync(directory).forEach(file_or_dir => {
     const filePath = path.resolve(directory, file_or_dir)
@@ -14,7 +21,7 @@ export const expressSetupRoutes = (app: Application) => {
       if (filesystem.statSync(route_path).isDirectory()) {
         filesystem
           .readdirSync(route_path)
-          .filter(file => file.endsWith('.routes.ts'))
+          .filter(file => file.endsWith(file_extension))
           .forEach(file => {
             const { routes } = require(path.join(route_path, file))
             app.use('/', routes)
